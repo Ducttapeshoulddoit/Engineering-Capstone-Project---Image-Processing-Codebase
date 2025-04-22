401cf23e5a593bed9682339b129acf4a176f328f
Ducttapeshoulddoit <110990778+Ducttapeshoulddoit@users.noreply.github.com>
2025-04-22 14:27:50 +1000

    Message processing - binarinsation image via histogram

--- Prototyping/messageDecoding.m
+++ Prototyping/messageDecoding.m
@@ -34,7 +34,37 @@ x_range = 0:255;
 y_fit = polyval(p, x_range);
 
 
+% Find the median to threshold the values
+
+%cumsum - returns sum of count - our y values.  Add up the amount of pixels
+%of the histogram/image then find the middle point 
+cumsum_histogram = cumsum(count);
+total_pixels = sum(count);
+median_index = find(cumsum_histogram >= total_pixels/2, 1, 'first');
+%jump to the median index found based on the binLocations/x values (0-256)
+% look at x values/binLocations array to find the median value using the
+% index found
+median = binLocations(median_index);
+disp(['Median intensity from histogram: ', num2str(median)]);
+
 figure;
 plot(binLocations, count)
 hold on;
 plot(x_range,y_fit)
+hold on;
+xline(median)
+
+% for values under the median, make black 1, values above make white 0
+% look at count and threshold the values under 125 
+
+binaryImage = stretched_img2 < 125;  % Values under 125 will be true (1)
+figure
+imshow(binaryImage)
+
+
+
+
+%%
+BW=imbinarize(stretched_img2);
+figure
+imshow(BW)
