b05384b7b6e306b29387699af32141a96149a315
Ducttapeshoulddoit <110990778+Ducttapeshoulddoit@users.noreply.github.com>
2025-04-28 16:54:55 +1000

    New circle detection pseudocode

--- Prototyping/detectCircleObjects.m
+++ Prototyping/detectCircleObjects.m
@@ -4,6 +4,8 @@
 image = imread("2025-Sem1/Capstone A/MATLAB" + ...
     "/images/Test images 10kHz transmission/_DSF0187.JPG");
 
+% Check the image is linear or SRGB - maybe later
+
 % grayscale and binarise the image
 GS = im2gray(image);
 BW = imbinarize(GS);
@@ -13,5 +15,15 @@ subplot(1,3,2); imshow(GS); title('Grayscale')
 subplot(1,3,3); imshow(BW); title('Binary') % prob not the best way
 
 % Gamma correction
+J = imadjust(GS,[],[],0.5);
+figure; imshowpair(image,J,"montage")
 
+%% New method - make 4 cursors that will trace across the image to find the
+% circle object.
 
+[widht, height] = size(J);
+
+% make 4 cursors - h (horziontal) , v (vert) , d1/d2 (diagonal 1/2)
+% D1 starts from 1,1 to the w,h while D2 starts from (w,1) to (1,h)
+
+% making 1 first
